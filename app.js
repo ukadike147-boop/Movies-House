a6abfde6bcf3639704ba7b7231b79599095d92fa
ukadike147-boop <ukadike147@gmail.com>
2026-06-28 13:35:28 -0500

    updated

--- app.js
+++ app.js
@@ -28,3 +28,8 @@ function cardFunc(img, title, rating) {
         </div>
     </div>`
 }
+const burgerBtn = document .getElementById("burgerBtn");
+const navLinks = document .getElementById("navLinks");
+burgerBtn.addEventListener("click", ()=> {
+    navLinks.classList.toggle("active");
+});
