990fd38824573e81f6228d792b85652d1893be2b
ukadike147-boop <ukadike147@gmail.com>
2026-06-29 10:03:39 -0500

    fix

--- app.js
+++ app.js
@@ -19,26 +19,35 @@ async function getMovieData(SearchTerm) {
     }
     const data = await getMovieData(SearchTerm);
 
-    if (!data.search) {
-        movieList.innerHTML = "<p> No movies found.</P>";
+    if (!data.Search) {
+        movielist.innerHTML = "<p> No movies found.</P>";
         return;
     }
-    movieList.innerHTML = data.search
+    movielist.innerHTML = data.Search
        .map(movie => cardFunc(movie.Poster, movie.Title, movie.Year))
        .join("");
 });
 
 function cardFunc(img, title, rating) {
-    return `<div class="card">
-        <img src="${img}" alt="" class="movie-img">
-        <div class="content">
-            <p>title: ${title}</p>
-            <p>Year: ${rating}</p>
-        </div>
-    </div>`
+    return
+    let movies = data.Search;
+    const sortSelect = document.getElementById("sortSelect");
+    if (sortSelect.value === "year-new") {
+        movies.sort((a, b) => b.Year - a.Year);
+    }
+    if (sortSelect.value === "year-old") {
+        movies.sort((a, b) => a.Year - b.Year);
+    }
+    if (sortSelect.value === "title") {
+        movies.sort((a, b) => a.Title.localeCompare(b.Title));
+    }
+    movielist.innerHTML = movies
+    .map(movie => cardFunc(movie.Poster,movie.Title, movie.Year))
+    .join("");
 }
-const burgerBtn = document .getElementById("burgerBtn");
-const navLinks = document .getElementById("navLinks");
-burgerBtn.addEventListener("click", ()=> {
+const burgerBtn = document.getElementById("burgerBtn");
+const navLinks = document.getElementById("navLinks");
+
+burgerBtn.addEventListener("click", function () {
     navLinks.classList.toggle("active");
 });
